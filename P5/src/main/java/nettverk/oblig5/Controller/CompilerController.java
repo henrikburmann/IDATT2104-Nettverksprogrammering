94912f2de6f151fbdb2c1089cb77e136214f8b8f
Marcus Johannessen <marcusjo@stud.ntnu.no>
2022-03-06 16:56:05 +0100

    working on compiler

---
+++ P5/src/main/java/nettverk/oblig5/Controller/CompilerController.java
@@ -0,0 +1,15 @@
+package nettverk.oblig5.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CompilerController {
+
+    @PostMapping("/compile")
+    @CrossOrigin
+
+
+}
---
+++ P5/src/main/java/nettverk/oblig5/service/Compiler.java
@@ -0,0 +1,12 @@
+package nettverk.oblig5.service;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.multipart.MultipartFile;
+
+public class Compiler {
+
+    public ResponseEntity<Object> compiler () throws Exception {
+
+        return null;
+    }
+}
