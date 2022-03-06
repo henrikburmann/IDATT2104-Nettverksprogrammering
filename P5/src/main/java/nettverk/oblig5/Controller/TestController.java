875f1556945061baef852b418e12191a36a4e804
Marcus Johannessen <marcusjo@stud.ntnu.no>
2022-03-06 16:22:53 +0100

    fixed loading of frontend

---
+++ P5/src/main/java/nettverk/oblig5/Controller/TestController.java
@@ -0,0 +1,10 @@
+package nettverk.oblig5.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+
+}
