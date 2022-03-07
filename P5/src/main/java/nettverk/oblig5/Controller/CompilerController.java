1169edd253839ecc3dd5248f2645aa316f7ad23a
Marcus Johannessen <marcusjo@stud.ntnu.no>
2022-03-07 12:24:11 +0100

    working on compiling and stuff

--- P5/src/main/java/nettverk/oblig5/Controller/CompilerController.java
+++ P5/src/main/java/nettverk/oblig5/Controller/CompilerController.java
@@ -1,15 +1,62 @@
 package nettverk.oblig5.Controller;
 
-import org.springframework.stereotype.Controller;
+import org.apache.tomcat.jni.Directory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Map;
 
 @RestController
 public class CompilerController {
 
         @PostMapping("/compile")
         @CrossOrigin
-        
+        public ResponseEntity<Object> compile(@RequestBody Map<String, String> data) throws IOException, InterruptedException {
 
+            if (!data.containsKey("code")) {
+                throw new IllegalArgumentException();
+            }
+
+            String code = data.get("code");
+
+            // File temp = new File("P5/src/main/temp");
+            Path dir = Files.createTempDirectory("P5/docker/temp");
+            System.out.println("Dir: " + dir.toString());
+            // boolean isWritten = temp.mkdir();
+
+            // System.out.println("Laget directory: " + isWritten);
+
+            FileWriter writer = new FileWriter("P5/docker/temp/eksempel.cpp");
+            writer.write(code);
+            writer.flush();
+
+            Runtime.getRuntime().exec("docker rmi cpp-compiler").waitFor();
+
+            Process process = Runtime.getRuntime().exec("docker build ./P5/ -t cpp-compiler");
+
+            String buildError = Arrays.toString(process.getErrorStream().readAllBytes());
+
+            // temp.deleteOnExit();
+
+            Process run = Runtime.getRuntime().exec("docker run --rm cpp-compiler");
+
+            String runError = Arrays.toString(run.getErrorStream().readAllBytes());
+
+            return ResponseEntity.status(HttpStatus.OK).body(makeMap("output", buildError + "\nProgram output:\n" + run));
+        }
+
+        private Map<String, String> makeMap(String key, String content) {
+            return Map.of(key, content);
+        }
 }
--- P5/src/main/resources/static/index.js
+++ P5/src/main/resources/static/index.js
@@ -1,7 +1,19 @@
-function trykk(){
+document.getElementById("form").addEventListener("submit", function (event){
+    event.preventDefault();
     console.log("mottatt")
-    let tekst = document.getElementById("kode").value;
+    let code = document.getElementById("kode").value;
 
-    console.log(tekst);
-    document.querySelector("#utskrevetKode").innerHTML = tekst;
-}
+    const url = "http://localhost:8080/compile"
+    const request = new XMLHttpRequest()
+
+    request.open("post", url)
+    request.setRequestHeader("content-type", "application/json; charset=utf-8")
+    request.responseType = "json"
+    request.onload = () => {
+        document.getElementById("output").value = request.response.output
+    }
+    request.send(JSON.stringify({"code":code}))
+
+    console.log(code);
+    document.querySelector("#utskrevetKode").innerHTML = code;
+})
